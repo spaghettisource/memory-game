36a2022dc9749d9935b9ca1ef4b754af561cdb11
spaghettisource <natureelement@gmail.com>
2019-05-16 17:15:51 +0200

    basic draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+
+  constructor () {
+    this.shuffleArray(this.cards);
+  }
+
+   shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+
 }
